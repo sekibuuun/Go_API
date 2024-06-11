6b07b1b5d9258c154f8d22b34fbf227d2067a55d
sekibuuun <akaunto.no.001@gmail.com>
2024-06-11 21:34:29 +0900

    sec6-3

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
 	"github.com/sekibuuun/go_api/controllers"
+	"github.com/sekibuuun/go_api/routers"
 	"github.com/sekibuuun/go_api/services"
 )
 
@@ -30,14 +30,7 @@ func main() {
 	ser := services.NewMyAppService(db)
 	con := controllers.NewMyAppController(ser)
 
-	r := mux.NewRouter()
-
-	r.HandleFunc("/hello", con.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", con.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
+	r := routers.NewRouter(con)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
