c26ae15ecfce3061511efcf154e90b130d440cd0
sekibuuun <akaunto.no.001@gmail.com>
2024-06-19 20:42:54 +0900

    sec9-3

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -5,11 +5,29 @@ import (
 	"net/http"
 )
 
+type resLoggingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
 // func(w http.ResponseWriter, req *http.Request) {} は無名関数
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		log.Println(req.RequestURI, req.Method)
 
-		next.ServeHTTP(w, req)
+		rlw := NewResLoggingWriter(w)
+
+		next.ServeHTTP(rlw, req)
+
+		log.Println("res: ", rlw.code)
 	})
+}
+
+func NewResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
+	return &resLoggingWriter{ResponseWriter: w, code: http.StatusOK}
+}
+
+func (rsw *resLoggingWriter) WriteHeader(code int) {
+	rsw.code = code
+	rsw.ResponseWriter.WriteHeader(code)
 }
