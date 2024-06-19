fcea2dbabf366aa4c10d9104da7f213eb9d41694
sekibuuun <akaunto.no.001@gmail.com>
2024-06-19 10:30:35 +0900

    sec9-2[wip]

---
+++ api/middlewares/logging.go
@@ -0,0 +1,15 @@
+package middlewares
+
+import (
+	"log"
+	"net/http"
+)
+
+// func(w http.ResponseWriter, req *http.Request) {} は無名関数
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		log.Panicln(req.RequestURI, req.Method)
+
+		next.ServeHTTP(w, req)
+	})
+}
