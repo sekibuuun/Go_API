7aa07cd01b257ca5aebd9211a36e4da026b1d732
sekibuuun <akaunto.no.001@gmail.com>
2024-06-24 13:21:43 +0900

    sec11-2

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -13,13 +13,14 @@ type resLoggingWriter struct {
 // func(w http.ResponseWriter, req *http.Request) {} は無名関数
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		log.Println(req.RequestURI, req.Method)
+		traceID := newTraceID()
 
+		log.Printf("[%d]%s %s\n", traceID, req.RequestURI, req.Method)
 		rlw := NewResLoggingWriter(w)
 
 		next.ServeHTTP(rlw, req)
 
-		log.Println("res: ", rlw.code)
+		log.Printf("[%d]res: %d\n", traceID, rlw.code)
 	})
 }
 
